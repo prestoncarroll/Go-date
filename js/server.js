cc06d6b4e3dd24664f6dc29993a2fb1307fd3efd
Preston Carroll <plutusinc1@yahoo.com>
2020-01-26 18:12:32 -0600

    updated repo

---
+++ js/server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var path = require("path");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+app.get("/", function (req, res) {
+    // res.send("Welcome to home Page!")
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/dating", function (req, res) {
+    // res.send("Welcome to home Page!")
+    res.sendFile(path.join(__dirname, "/dating.html"));
+});
+app.get("/signin", function (req, res) {
+    // res.send("Welcome to home Page!")
+    res.sendFile(path.join(__dirname, "/signin.html"));
+});
+app.get("/createaccount", function (req, res) {
+    // res.send("Welcome to home Page!")
+    res.sendFile(path.join(__dirname, "/createaccount.html"));
+
+});
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
